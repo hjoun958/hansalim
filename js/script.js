b974bd05808d74bc28c2132eb4415799d73116d5
jeong ji hyeon <89637853+hjoun958@users.noreply.github.com>
2021-11-24 12:45:38 +0900

    기본 코드 수정 업로드
    기본 코드 수정 업로드

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 // html 이 준비가 되면 실행한다.
 $(document).ready(function () {
-
+    // 모달창
+    let modal = $('.modal');
+    let modal_cont = $('.modal-cont');
+    let modal_close = $('.modal-close');
+    let modal_show = $('.modal-show');
+    modal_close.click(function () {
+        modal.fadeOut();
+    });
+    modal.click(function () {
+        modal.fadeOut();
+    });
+    modal_cont.click(function (event) {
+        event.stopPropagation();
+    });
+    modal_show.click(function (e) {
+        // href 를 막아야 합니다.
+        e.preventDefault();
+        modal.fadeIn();
+    });
     // 상단 고정 메뉴 기능
     let scroll_y = $(window).scrollTop();
     let header = $('.header');
